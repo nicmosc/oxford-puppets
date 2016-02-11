104263507bfaa48b9a339c0cac5012e800c84470
ironvital <ironvital0@gmail.com>
2016-02-11 17:35:07 +0000

    Maven

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by user on 11/02/16.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isEmpty("Hello World!"));
+    }
+
+}
